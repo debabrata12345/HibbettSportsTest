d4f31683cd9d460faff43cef7a2970019c15d7a0
deba <debabrata@heady.io>
2019-10-17 17:54:57 +0530

    Testcase added

--- src/main/java/com/hibbett/base/TestCaseName.java
+++ src/main/java/com/hibbett/base/TestCaseName.java
@@ -2,13 +2,13 @@ package com.hibbett.base;
 
 public interface TestCaseName {
 
-// LoginModule TestCases
+// LoginModule TestCases-4
 	String TESTCASE1 = "To validate Login button of Landing screen is opening the Login screen";
 	String TESTCASE2 = "To validate that email and password is required field";
 	String TESTCASE3 = "To validate invalid Login validation";
 	String TESTCASE4 = "To validate Login functionality with valid login";
 
-// SIGNUPModule TestCases	
+// SIGNUPModule TestCases-4	
 	String TESTCASE5 ="To validate SignUp button of Landing screen is opening the Signup screen";
 	String TESTCASE6 ="To validate mandatory field validations present on the Signup form";
 	String TESTCASE7 ="To validate Login button present in SignUp screen is opening the Login screen";
@@ -16,7 +16,7 @@ public interface TestCaseName {
 	
 	
 	
-//Account>AboutUs
+//Account>AboutUs-11
 	
 	String TESTCASE101 ="To validate Open Source License option is opening Open Source Libraries page";
 	String TESTCASE102 ="To validate Accessibility option is opening Accessibility page";
@@ -30,8 +30,29 @@ public interface TestCaseName {
 	String TESTCASE110 ="To validate HibbettUniversity option is opening HibbettUniversity page";
 	String TESTCASE111 ="To validate OurStoryoption is opening OurStory page";
 	
+//Account>Customer Support-4
+	String TESTCASE112 ="To validate FAQ option is opening the correct page";
+	String TESTCASE113 ="To validate Email Us  option is opening the correct page";
+	String TESTCASE114 ="To validate Call Us option is opening the correct page";
+	String TESTCASE115 ="To validate App Feedback  option is opening the correct page";
 	
-//ShopTab	
+//Account>Account Settings-6
+	String TESTCASE116 ="To validate My Personal Info edit feature";
+	String TESTCASE117 ="To validate Add a card feature feature";
+	String TESTCASE118 ="To validate Delete a card feature";
+	String TESTCASE119 ="To validate Add/Edit/Delete an address feature";
+	String TESTCASE120 ="To validate Change Password feature";
+	String TESTCASE121 ="To validate Logout feature";
+	
+//Account>Raffle Status-5
+	String TESTCASE122 ="To validate My Launches/Registered item list ot PDP navigation";
+	String TESTCASE123 ="To validate My Launches/Wins item list ot PDP navigation";
+	String TESTCASE124 ="To validate user is able to access Hibbett Rewards section";
+	String TESTCASE125="To validate Find a Store option is opening correct page";
+	String TESTCASE126="To validate Order History option is opening correct page";
+	
+	
+//ShopTab-10
 	String TESTCASE200 ="To validate SNEAKERS-NIKE-AIRFORCE1 promotional tile is opening correct page";
 	String TESTCASE201 ="To validate SNEAKERS-ADIDAS-SUPERSTAR promotional tile is opening correct page";
 	String TESTCASE203="To validate View All feature of Launch and Raffle section";
@@ -39,20 +60,30 @@ public interface TestCaseName {
 	String TESTCASE205="To Validate navigating to PLP from any of the tile displayed";
 	String TESTCASE206="To validate store selection feature";
 	String TESTCASE207="To validate store specific categories are displayed when store checkbox is checked";
+	String TESTCASE208 ="To validate socks tile is opening socks plp screen";
+	String TESTCASE209 ="To validate all promotional tile is opening PLP";
+	String TESTCASE210 ="To validate search and sort feature in PLP";
 	
-//SMS_Tab
+	
+//SMS_Tab-5
 	String TESTCASE300="To validate fresh launch, More Info button functionality";
 	String TESTCASE301="To valite the GET STARTED button functionality";
 	String TESTCASE302="To validate store setting feature";
 	String TESTCASE303="To validate store edit feature";
 	String TESTCASE304="To validate store specific categories are displayed when store checkbox is checked";
 	
+
+//Launches_Tab-3
 	
+	String TESTCASE401="To validate raffle entry feature";
+	String TESTCASE402="To validate Grid and List view";
+	String TESTCASE403="To validate Launched tap PDP navigation";
 	
-	
-	
-	
-	String TESTCASE3301 ="To validate socks tile is opening socks plp screen";
+//Bag-
+	String TESTCASE500="To validate Ecomm checkout feature";
+	String TESTCASE501="To validate BOPIS checkout feature";
+	String TESTCASE502="To validate ROPIS checkout feature";
+	String TESTCASE503="To validate changing eComm to BOPIS and viceversa";
 	
 	
 }
